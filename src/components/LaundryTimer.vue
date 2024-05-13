b03a7925c85e111a560f546a262bfa2267632bfe
Sebastian Böhm <mail@sboe0705.de>
2024-05-13 22:59:37 +0200

    added calculation of finishing time, added date-fns

--- src/components/LaundryTimer.vue
+++ src/components/LaundryTimer.vue
@@ -1,37 +1,44 @@
 <script>
+import { format, formatDistanceToNowStrict } from 'date-fns'
 export default {
   data() {
     return {
+
+      // technical variables
+      modalIsActive: false,
+      modalCanClose: ['escape', 'outside'],
+
+      // functional variables
       washPrograms: [
         {id: '1', label: '2:00 Hours', duration: 120},
         {id: '2', label: '2:30 Hours', duration: 150}
       ],
-      finishingTime: null,
-      modalActive: 'modal'
+      timeBuffer: 15,
+      washProgramDuration: null,
+      finishingTime: null
     }
   },
   methods: {
-    changeMessage() {
-      this.displayedMessage = this.userInput;
-    },
-    openModal() {
-      this.modalActive = 'modal is-active';
-    },
-    closeModal() {
-      this.modalActive = 'modal';
+    calculateTimers() {
+      if (this.washProgramDuration != null && this.finishingTime != null) {
+        this.finishingTimeAsString = format(this.finishingTime, "p");
+        this.durationTillFinishedAsString = formatDistanceToNowStrict(this.finishingTime);
+
+        this.modalIsActive = true;
+      }
     }
   }
 }
 </script>
 
 <template>
-    <div class="container">
+    <div class="container is-fluid">
 
     <p class="title is-1">Laundry Timer</p>
 
     <b-field label="Wash Program" label-position="on-border">
-      <b-select expanded placeholder="Select Wash Program ...">
-        <option v-for="option in washPrograms" :key="option.id" :value="option.id">
+      <b-select expanded placeholder="Select Wash Program ..." v-model="washProgramDuration">
+        <option v-for="option in washPrograms" :key="option.id" :value="option.duration">
           {{ option.label }}
         </option>
       </b-select>
@@ -41,30 +48,13 @@ export default {
       <b-timepicker placeholder="Select Finishing Time ..." :increment-minutes="15" v-model="finishingTime" />
     </b-field>
 
-    <b-button type="is-primary" @click="openModal">Calculate Timers</b-button>
+    <b-button type="is-primary" @click="calculateTimers">Calculate Timers</b-button>
 
-    <div :class="modalActive">
-      <div class="modal-background" @click="closeModal" />
-      <div class="modal-content">
-        <div class="container">
-          <article class="message is-primary">
-            <div class="message-header">
-              <p>Hello World</p>
-              <button class="delete" aria-label="delete" @click="closeModal" />
-            </div>
-            <div class="message-body">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec
-              nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus
-              diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac
-              <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et
-              sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a
-              neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
-            </div>
-          </article>
-        </div>
-      </div>
-    </div>
+    <b-modal v-model="modalIsActive" :can-cancel="modalCanClose">
+      <b-message title="Success" type="is-success" aria-close-label="Close message" @close="modalIsActive = false">
+        The laundry is finished in <b>{{durationTillFinishedAsString}}</b> (at {{finishingTimeAsString}}).
+      </b-message>
+    </b-modal>
 
   </div>
 </template>
